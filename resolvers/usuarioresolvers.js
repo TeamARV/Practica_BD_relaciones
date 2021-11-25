c80853f11094f67fd5603e38a1112e6228fd1cf1
TeamARV <andresriveravega@msn.com>
2021-11-24 19:17:21 -0500

    se agrega all resolvers mutaciones para usuario

--- resolvers/usuarioresolvers.js
+++ resolvers/usuarioresolvers.js
@@ -6,11 +6,52 @@ module.exports.usuarioresolvers = {
       test: () => 'hola desde usuario',
 
       Usuarios: async (parent, args) => {
-        const usuarios = await Usuario.find();
-        return usuarios;
+        if (Object.keys(args).includes('_id')) {
+        const usuarios = await Usuario.find({_id:args._id});
+        return usuarios;}
+        else{const usuarios = await Usuario.find();
+        return usuarios;}
       },
       
 
+    },
+
+  
+
+    Mutation: {
+
+      crearUsuario: async (parents,args) => {
+         const usuariocreado =  await Usuario.create({
+          correo : args.correo,
+          identificacion : args.identificacion,
+          nombreCompleto : args.nombreCompleto,
+          clave : args.clave,
+          tipoUsuario : args.tipoUsuario,
+          estado : args.estado,
+         })
+         return usuariocreado},
+
+
+         editarUsuario: async (parent, args) => {
+          const usuarioEditado = await Usuario.findByIdAndUpdate(
+            args._id,
+            {
+              correo : args.correo,
+              identificacion : args.identificacion,
+              nombreCompleto : args.nombreCompleto,
+              clave : args.clave,
+              tipoUsuario : args.tipoUsuario,
+              estado : args.estado,
+            },
+            { new: true }
+          );
+    
+          return usuarioEditado;},
+
+          eliminarUsuario: async (parent, args) => {
+              const usuarioDeleteado = await Usuario.findOneAndDelete({ _id: args._id });
+              return usuarioDeleteado;},
+        
     },
 
 
--- typeDefs/usuariotypeDefs.js
+++ typeDefs/usuariotypeDefs.js
@@ -15,7 +15,36 @@ module.exports.usuariotypeDefs = gql`
 type Query {
   "A simple type for getting started!"
   test: String
-  Usuarios: [Usuario]
+  Usuarios(_id:ID): [Usuario]
+}
+
+type Mutation{
+
+  crearUsuario(
+    correo: String
+    identificacion: String
+    nombreCompleto: String
+    clave: String
+    tipoUsuario: String
+    estado: String
+  
+  ):Usuario
+
+  editarUsuario(
+    _id:ID!
+    correo: String
+    identificacion: String
+    nombreCompleto: String
+    clave: String
+    tipoUsuario: String
+    estado: String
+  
+  ):Usuario
+
+  eliminarUsuario(
+    _id:ID!
+  ):Usuario
+
 }
 `;
 
